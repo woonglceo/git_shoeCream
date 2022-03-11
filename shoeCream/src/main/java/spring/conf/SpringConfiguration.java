0d131b05c38f6edeaa0d19ef285f099ca6831121
sohee <jangsh229@gmail.com>
2022-03-11 15:05:48 +0900

    springConfiguration.java 수정

--- shoeCream/src/main/java/spring/conf/SpringConfiguration.java
+++ shoeCream/src/main/java/spring/conf/SpringConfiguration.java
@@ -1,5 +1,7 @@
 package spring.conf;
 
+import java.util.Properties;
+
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -14,68 +16,95 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
 @PropertySource("classpath:spring/db.properties")
 public class SpringConfiguration {
-	private @Value("${jdbc.driver}") String driver;
-	private @Value("${jdbc.url}") String url;
-	private @Value("${jdbc.username}") String username;
-	private @Value("${jdbc.password}") String password;
-	
-	@Bean
-	public BasicDataSource dataSource(){
-		BasicDataSource basicDataSource = new BasicDataSource();
-		basicDataSource.setDriverClassName(driver);
-		basicDataSource.setUrl(url);
-		basicDataSource.setUsername(username);
-		basicDataSource.setPassword(password);
-		basicDataSource.setMaxTotal(20);
-		basicDataSource.setMaxIdle(3);
-		
-		return basicDataSource;
-	}
-	
-	@Bean
-	public SqlSessionFactory sqlSessionFactory() throws Exception {
-		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-		sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("spring/mybatis-config.xml"));
-		sqlSessionFactoryBean.setDataSource(dataSource());
-		sqlSessionFactoryBean.setMapperLocations(
-				  new ClassPathResource("stock/dao/stockMapper.xml")
-				, new ClassPathResource("content/dao/contentMapper.xml")
-				, new ClassPathResource("product/dao/productMapper.xml")
-				, new ClassPathResource("deal/dao/dealMapper.xml")
-				, new ClassPathResource("order/dao/orderMapper.xml")
-				, new ClassPathResource("serviceCenter/dao/serviceCenterMapper.xml")
-				, new ClassPathResource("style/dao/styleMapper.xml")
-				, new ClassPathResource("user/dao/userMapper.xml")
-				, new ClassPathResource("event/dao/eventMapper.xml")
-				, new ClassPathResource("mypage/dao/mypageMapper.xml")
-				
-//				new PathMatchingResourcePatternResolver().getResources("classpath:**/dao/*Mapper.xml")
-//			  , new PathMatchingResourcePatternResolver().getResources("classpath:serviceCenter/dao/serviceCenterMapper.xml")
-				
-//				new ClassPathResource("admin/*/dao/*Mapper.xml")
+   private @Value("${jdbc.driver}") String driver;
+   private @Value("${jdbc.url}") String url;
+   private @Value("${jdbc.username}") String username;
+   private @Value("${jdbc.password}") String password;
+   
+   @Bean
+   public BasicDataSource dataSource(){
+      BasicDataSource basicDataSource = new BasicDataSource();
+      basicDataSource.setDriverClassName(driver);
+      basicDataSource.setUrl(url);
+      basicDataSource.setUsername(username);
+      basicDataSource.setPassword(password);
+      basicDataSource.setMaxTotal(20);
+      basicDataSource.setMaxIdle(3);
+      
+      return basicDataSource;
+   }
+   
+   @Bean
+   public SqlSessionFactory sqlSessionFactory() throws Exception {
+      SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+      sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("spring/mybatis-config.xml"));
+      sqlSessionFactoryBean.setDataSource(dataSource());
+      sqlSessionFactoryBean.setMapperLocations(
+              new ClassPathResource("stock/dao/stockMapper.xml")
+            , new ClassPathResource("content/dao/contentMapper.xml")
+            , new ClassPathResource("product/dao/productMapper.xml")
+            , new ClassPathResource("deal/dao/dealMapper.xml")
+            , new ClassPathResource("order/dao/orderMapper.xml")
+            , new ClassPathResource("serviceCenter/dao/serviceCenterMapper.xml")
+            , new ClassPathResource("style/dao/styleMapper.xml")
+            , new ClassPathResource("user/dao/userMapper.xml")
+            , new ClassPathResource("event/dao/eventMapper.xml")
+            , new ClassPathResource("mypage/dao/mypageMapper.xml")
+            
+//            new PathMatchingResourcePatternResolver().getResources("classpath:**/dao/*Mapper.xml")
+//           , new PathMatchingResourcePatternResolver().getResources("classpath:serviceCenter/dao/serviceCenterMapper.xml")
+            
+//            new ClassPathResource("admin/*/dao/*Mapper.xml")
 
-		);
-		
-		return sqlSessionFactoryBean.getObject();
-	}
-	
-	@Bean
-	public SqlSession sqlSession() throws Exception {
-		return new SqlSessionTemplate(sqlSessionFactory());
-	}
-	
-	@Bean
-	public DataSourceTransactionManager transactionManager() {
-		return new DataSourceTransactionManager(dataSource());
-	}
+      );
+      
+      return sqlSessionFactoryBean.getObject();
+   }
+   
+   @Bean
+   public SqlSession sqlSession() throws Exception {
+      return new SqlSessionTemplate(sqlSessionFactory());
+   }
+   
+   @Bean
+   public DataSourceTransactionManager transactionManager() {
+      return new DataSourceTransactionManager(dataSource());
+   }
+   
+   /* 메일 인증 관련 */
+   @Configuration
+   public class MailAuthConfiguration {
+      @Bean(name="mailSender")
+      public JavaMailSender getJavaMailSender() {
+         Properties properties = new Properties(); 
+         properties.put("mail.smtp.auth", true);
+         properties.put("mail.transport.protocol", "smtp");
+         properties.put("mail.debug", true);
+         properties.put("mail.smtp.starttls.enable", true);
+         properties.put("mail.smtp.starttls.trust", true);
+         properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
+         
+         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+         mailSender.setHost("smtp.gmail.com");
+         mailSender.setPort(587);
+         mailSender.setUsername("xxhyeeun@gmail.com"); /* 관리자 이메일 */
+         mailSender.setPassword("mint1111*"); /* 관리자 비밀번호 */
+         mailSender.setDefaultEncoding("utf-8");
+         mailSender.setJavaMailProperties(properties);
+         
+         return mailSender;
+         
+      }
+   }
 }
-
 
 
 
